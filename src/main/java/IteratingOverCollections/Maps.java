79e72337ff96181f3dd663ed6b5b65988d4263e1
Rakesh Choudhary <rakeshchoudhary21@outlook.com>
2019-01-30 21:00:26 +0530

    Map Update- computeIfAbsent, Present, and merge

--- src/main/java/IteratingOverCollections/Maps.java
+++ src/main/java/IteratingOverCollections/Maps.java
@@ -31,5 +31,27 @@ public class Maps {
         System.out.println(map.remove(3,"SOME_OTHER_THING"));
         //same way a new replace, which replaces only if given key is associated with given value
         System.out.println(map.replace(1,"ONE","ONDU"));
+
+
+        //You can replace a value with compute method as well
+        map.compute(1,(key,value)-> value+"_NEW_VAL");
+        //diff between + and concat is that + doesn't throw NPE
+        //also concat takes only one arg and of type string, but + can take any type of argument.
+        map.compute(1,(key,value)-> value.concat("_NEW_VAL"));
+        //compute will do simply add the key to map if key doesnt exists, value for this key will be the result of computation.
+        map.compute(-9,(key,value) -> "-9");
+        //Also note if computation returns null then that key is removed from the map.
+        System.out.println(map.get(-9)); //this will print -9
+        map.compute(-9,(k,v) -> null);
+        System.out.println(map.keySet()); //this will not contain -9 now
+        map.computeIfPresent(-9,(k,v) -> null);
+        System.out.println(map.keySet()); //this will not contain -9 now
+        map.computeIfAbsent(-9,k-> "NEW_VALUE");
+        System.out.println(map.keySet());
+        //or use merge
+        //but for merge both key and value must match, then the remap function is applied.
+        //if key-> val pair is not found then the value at key is simply replaced
+        map.merge(1,"ONDU_NEW_VAL",(key,value) -> value);
+        System.out.println(map.get(1));
     }
 }
